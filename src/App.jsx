bd9b8fe33ffd9db94fc30ebf82313b37e5ffed4d
isabella <isa99fv@gmail.com>
2024-07-07 19:49:45 -0400

    add routes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import './App.scss'
+import "./App.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Events from "./pages/Events";
+import GetInvolved from "./pages/GetInvolved";
 
 function App() {
   return (
-    <>
-      <p>this is App.jsx</p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/get-involved" element={<GetInvolved />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
