4fef9910c0c5d10d740a691790ed62385f9a0ca1
isabella <isa99fv@gmail.com>
2024-07-09 13:28:15 -0400

    add hero content and styles for mobile and tablet

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import "./Hero.scss"
+
+const Hero = () => {
+  return (
+    <section className="hero">
+        <h1 className="hero__title">Celebrating & Fostering Pride in the KW Region</h1>
+        <button className="hero__button button">NEXT EVENT</button>
+    </section>
+  )
+}
+
+export default Hero
