7f6ce75d810f0bdb791c0ac258fd76649bfeb2dc
isabella <isa99fv@gmail.com>
2024-07-12 11:25:37 -0400

    next event button in hero redirects to /events

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,11 @@
 import "./Hero.scss"
+import { Link } from "react-router-dom"
 
 const Hero = () => {
   return (
     <section className="hero">
         <h1 className="hero__title">Celebrating & Fostering Pride in the KW Region</h1>
-        <button className="hero__button button">NEXT EVENT</button>
+        <Link to="/events" className="hero__button button">NEXT EVENT</Link>
     </section>
   )
 }
